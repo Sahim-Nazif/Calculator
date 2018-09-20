dce120030f01baf93c925e29d3de9fb2c7cd2036
Sahim Nazif <nsahim@my.centennialcollege.ca>
2018-09-20 17:54:38 -0400

    Methods called with values in main

--- Calculator/src/Calculator.java
+++ Calculator/src/Calculator.java
@@ -1,20 +1,26 @@
 
 public class Calculator {
-		
-public int add(int a, int b) {
+
+
+public static int add(int a, int b) {
 	return a+b;
+
 }
-public int substract (int a,int b) {
+public static int subtract (int a,int b) {
 	return a-b;
 }
-public int multiple (int a, int b) {
+public static int multiple (int a, int b) {
 	return a *b;
 }
-public int divide (int a, int b) {
+public static  int divide (int a, int b) {
 	return a/b;
 }
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+			
+		System.out.printf("The sum result  of the two number is %d\n",add(12,16));
+		System.out.printf("The subtraction result  of the two number is %d\n",subtract(30,14));
+		System.out.printf("The mulipication result  of the two number is %d\n",multiple(10,5));
+		System.out.printf("The division result  of the two number is %d\n",divide(25,5));
 
 	}
 
