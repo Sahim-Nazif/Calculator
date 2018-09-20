02b9ca87e57074dea1205492302cfed49bbf5dfe
Sahim Nazif <nsahim@my.centennialcollege.ca>
2018-09-20 17:31:56 -0400

    Defined methods

--- Calculator/src/Calculator.java
+++ Calculator/src/Calculator.java
@@ -1,7 +1,18 @@
 
 public class Calculator {
 		
-
+public int add(int a, int b) {
+	return a+b;
+}
+public int substract (int a,int b) {
+	return a-b;
+}
+public int multiple (int a, int b) {
+	return a *b;
+}
+public int divide (int a, int b) {
+	return a/b;
+}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
